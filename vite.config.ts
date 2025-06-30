43b8b6a293ef73bcb7ae4a215684a167b668a5ed
Mario Brusarosco <mariobrusarosco@hotmail.com>
2025-06-30 00:25:41 -0300

    Lint Errors

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths({
+      projects: [path.resolve(__dirname, 'tsconfig.json')],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+  ],
+});
