14b8379792c8c8147fb73b2703e536cd1ce6be56
Mario Brusarosco <mariobrusarosco@hotmail.com>
2026-03-15 17:59:01 -0300

    feat: Introduce `useUpcomingSubscriptions` hook for data orchestration and enhance `UpcomingSubscriptions` component with loading, error, and empty states.

--- src/domains/dashboard/screens/index.tsx
+++ src/domains/dashboard/screens/index.tsx
@@ -3,20 +3,7 @@ import { UpcomingSubscriptions } from '../components/upcoming-subscriptions';
 export const DashboardIndexScreen = () => {
   return (
     <div data-test-id="dashboard-index-screen" className="flex flex-wrap gap-x-8 gap-y-12 py-6 pr-6">
-      {/* <div className="spinner w-20 h-20 text-center rounded-full" data-ui="spinner">
-          <div className="spinner1" />
-        </div> */}
-
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
-      <UpcomingSubscriptions />
       <UpcomingSubscriptions />
     </div>
   );
 };
-
